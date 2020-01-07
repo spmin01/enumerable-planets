0bb9c41da047d7cb1406aa604c6acbaa517651ca
Shaun Minnihan <spminnihan@gmail.com>
2020-01-06 19:32:30 -0600

    doneeeee

---
+++ src/main/java/org/launchcode/enumerableplanets/data/Planets.java
@@ -0,0 +1,26 @@
+package org.launchcode.enumerableplanets.data;
+
+public enum Planets {
+    MERCURY("Mercury", 88),
+    VENUS("Venus", 255),
+    EARTH("Earth", 365),
+    MARS("Mars", 687),
+    JUPITER("Jupiter", 4333),
+    SATURN("Saturn", 10759),
+    URANUS("Uranus", 30687),
+    NEPTUNE("Neptune", 60200);
+
+    private final String displayName;
+    private final int yearLength;
+
+    Planets(String displayName, int yearLength) {
+        this.displayName = displayName;
+        this.yearLength = yearLength;
+    }
+
+    public String getDisplayName() { return this.displayName; }
+
+    public int getYearLength() {
+        return yearLength;
+    }
+}
